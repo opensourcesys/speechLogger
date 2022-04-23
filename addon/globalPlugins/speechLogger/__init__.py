af540d314526855215387d79a6e23ec84caa82ac
Luke Davis <XLTechie+github@newanswertech.com>
2022-04-23 00:34:03 -0400

    Corrected file title and copyright in __init__.py.

--- addon/globalPlugins/speechLogger/__init__.py
+++ addon/globalPlugins/speechLogger/__init__.py
@@ -1,6 +1,6 @@
-# Speech Logger with Remote Support, V22.0
+# NVDA Speech Logger add-on, V22.0
 #
-#    Copyright (C) 2022 Luke Davis <XLTechie@newanswertech.com>
+#    Copyright (C) 2022 Luke Davis <XLTechie@newanswertech.com>, James Scholes
 #
 # This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License
 # as published by    the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
