b5bc1f785f772774b6b463726ca3e9f61d78e522
Luke Davis <XLTechie+github@newanswertech.com>
2024-09-29 22:58:38 -0400

    Trying to move new class, and initialize without super.

--- addon/globalPlugins/speechLogger/configUI.py
+++ addon/globalPlugins/speechLogger/configUI.py
@@ -11,6 +11,7 @@
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 import wx
+from wx.lib.expando import ExpandoTextCtrl
 from typing import Any
 
 import addonHandler
@@ -19,10 +20,11 @@ import ui
 import globalVars
 import gui
 from gui.settingsDialogs import PANEL_DESCRIPTION_WIDTH
+from gui.dpiScalingHelper import scaleSize
+from gui import guiHelper
 from logHandler import log
 
 from . import extensionPoint
-from .slPathSelectionHelper import SLPathSelectionHelper
 
 try:
 	addonHandler.initTranslation()
@@ -290,4 +292,29 @@ class SpeechLoggerSettings(gui.settingsDialogs.SettingsPanel):
 			self.customSeparatorControl.Enable(True)
 		else:
 			self.customSeparatorControl.Enable(False)
+
+
+class SLPathSelectionHelper(guiHelper.PathSelectionHelper):
+	"""
+	Abstracts away details for creating a path selection helper. The path selection helper is a textCtrl with a
+	button in horizontal layout. The Button launches a directory explorer. To get the path selected by the user, use the
+	`pathControl` property which exposes a wx.TextCtrl.
+	"""
+
+	def __init__(self, parent: wx.Dialog, buttonText: str, browseForDirectoryTitle: str) -> None:
+		"""
+		:param parent: An instance of the parent wx window. EG wx.Dialog
+		:param buttonText: The text for the button to launch a directory dialog (wx.DirDialog). This is typically 'Browse'
+		:param browseForDirectoryTitle: The text for the title of the directory dialog (wx.DirDialog)
+		"""
+		object.__init__(self)
+		self._textCtrl = ExpandoTextCtrl(
+			parent,
+			size=(scaleSize(250), -1),
+			style=wx.TE_READONLY,
+		)
+		self._browseButton = wx.Button(parent, label=buttonText)
+		self._browseForDirectoryTitle = browseForDirectoryTitle
+		self._browseButton.Bind(wx.EVT_BUTTON, self.onBrowseForDirectory)
+		self._sizer = associateElements(self._textCtrl, self._browseButton)
 
